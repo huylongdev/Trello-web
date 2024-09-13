e74dbf3e24b8ef9f77d8f4286fbb1961ddc69547
huylongdev <haolong2004crtt@gmail.com>
2024-09-13 11:07:15 +0700

    Add CSS baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Button from "@mui/material/Button";
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import Button from '@mui/material/Button'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import TabIndicator from '@mui/material/Tabs'
 
 
 function App() {
   return (
     <>
-      <div>huylongdev</div>
+      <TabIndicator />
+      <div> huylongdev </div>
       <Button variant="contained">Hello world</Button>
       <AccessAlarmIcon />
       <ThreeDRotation />
